fb1f45da203722fe2e3870fa36678722d936fb1d
jimmy0524 <10jmin04@naver.com>
2024-10-08 15:50:24 +0900

    1008 코테첼 Day2

---
+++ CoTeChallenge/day2_4963.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class day2_4963 {
+    static int[][] arr;
+    static boolean[][] visited;
+    static int W;
+    static int H;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int answer = 0;
+            W = Integer.parseInt(st.nextToken()); //너비
+            H = Integer.parseInt(st.nextToken()); //높이
+            if (W == 0 && H == 0) {
+                return;
+            }
+            arr = new int[H + 2][W + 2];
+            visited = new boolean[H + 2][W + 2];
+            for (int i = 1; i <= H; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= W; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 1; i <= H; i++) {
+                for (int j = 1; j <= W; j++) {
+                    if (arr[i][j] == 1) {
+                        if (!visited[i][j]) {
+                            BFS(i, j);
+                            answer += 1;
+                        }
+                    }
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+
+    public static void BFS(int i, int j) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, j});
+        visited[i][j] = true;
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int A = now[0];
+            int B = now[1];
+            if (i <= H && j <= W && A >= 1 && B >= 1) {
+                if (!visited[A + 1][B]) {
+                    if (arr[A + 1][B] == 1) {
+                        q.add(new int[]{A + 1, B});
+                        visited[A + 1][B] = true;
+                    }
+                }
+                if (!visited[A - 1][B]) {
+                    if (arr[A - 1][B] == 1) {
+                        q.add(new int[]{A - 1, B});
+                        visited[A - 1][B] = true;
+                    }
+                }
+                if (!visited[A][B + 1]) {
+                    if (arr[A][B + 1] == 1) {
+                        q.add(new int[]{A, B + 1});
+                        visited[A][B + 1] = true;
+                    }
+                }
+                if (!visited[A][B - 1]) {
+                    if (arr[A][B - 1] == 1) {
+                        q.add(new int[]{A, B - 1});
+                        visited[A][B - 1] = true;
+                    }
+                }
+                if (!visited[A - 1][B - 1]) {
+                    if (arr[A - 1][B - 1] == 1) {
+                        q.add(new int[]{A - 1, B - 1});
+                        visited[A - 1][B - 1] = true;
+                    }
+                }
+                if (!visited[A + 1][B + 1]) {
+                    if (arr[A + 1][B + 1] == 1) {
+                        q.add(new int[]{A + 1, B + 1});
+                        visited[A + 1][B + 1] = true;
+                    }
+                }
+                if (!visited[A + 1][B - 1]) {
+                    if (arr[A + 1][B - 1] == 1) {
+                        q.add(new int[]{A + 1, B - 1});
+                        visited[A + 1][B - 1] = true;
+                    }
+                }
+                if (!visited[A - 1][B + 1]) {
+                    if (arr[A - 1][B + 1] == 1) {
+                        q.add(new int[]{A - 1, B + 1});
+                        visited[A - 1][B + 1] = true;
+                    }
+                }
+            }
+        }
+    }
+}
