ea2e5bfb23300dcbce5dd51a0cf5660eaf407706
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-02-23 23:17:44 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,20 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-        int answer = 0;
-        for (int i = 0; i < 10; i++) {
-            boolean b = false;
-            for (int num : numbers) {
-                if (i == num) {
-                    b = true;
-                    break;
-                }
-            }
-            if (!b) {
-                answer += i;
-            }
+        int answer = 45;
+        
+        for (int num : numbers) {
+            answer -= num;
         }
+        
         return answer;
     }
 }
