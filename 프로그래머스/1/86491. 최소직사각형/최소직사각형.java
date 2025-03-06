1dc3e858f2462d2542b192b19c4de0b07d1c2c86
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-06 21:29:46 +0900

    [level 1] Title: 최소직사각형, Time: 1.82 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -4,15 +4,9 @@ class Solution {
     public int solution(int[][] sizes) {
         int wMax = 0;
         int hMax = 0;
-        for (int i = 0; i < sizes.length; i++) {
-            Arrays.sort(sizes[i]);
-            for (int j = 0; j < sizes[i].length; j++) {
-                if (j == 0) {
-                    wMax = Math.max(wMax, sizes[i][j]);
-                } else {
-                    hMax = Math.max(hMax, sizes[i][j]);
-                }
-            }
+        for (int[] now : sizes) {
+            wMax = Math.max(wMax, Math.max(now[0], now[1]));
+            hMax = Math.max(hMax, Math.min(now[0], now[1]));
         }
         return wMax * hMax;
     }
