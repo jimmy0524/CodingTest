31c4aaf4753d20abcdaad898b327b180f5b53d73
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-10 23:30:11 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,13 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int weight = 0;
-        int height = 0;
-        for (int i = 3; i < brown + yellow; i++) {
+        for (int i = 3; i <= (int) Math.sqrt(brown + yellow); i++) {
            if ((brown + yellow) % i == 0) {
                int j = (brown + yellow) / i;
                if ((i - 2) * (j - 2)  == yellow) {
-                   if (i > j) {
-                       weight = i;
-                       height = j;
-                   }
-                   height = i;
-                   weight = j;
-                   break;
+                   return new int[]{j, i};
                }
-               
            }
         }
-        return new int[]{weight, height};
+        return new int[]{0};
     }
 }
