bc2f001ffdec95701a72741521ecb12f0fb4d02a
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-12 19:34:02 +0900

    [level 1] Title: 콜라 문제, Time: 32.45 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n >= a) {
+            int add = (n / a) * b; // 6
+            int minus = (n / a) * a; // 18
+            System.out.println("add = " + add + " minus = " + minus);
+            answer += add;
+            n = n - minus + add;
+        }
+        return answer;
+    }
+}
