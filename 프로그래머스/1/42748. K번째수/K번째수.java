e91d7add014eef057a78316306039f5637d674c8
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-12 17:31:12 +0900

    [level 1] Title: K번째수, Time: 1.27 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,12 +5,9 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            ArrayList<Integer> temp = new ArrayList<>();
-            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
-                temp.add(array[j]);
-            }
-            Collections.sort(temp);
-            answer[i] = temp.get(commands[i][2] - 1);
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
         }
         return answer;
     }
