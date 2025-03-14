6e5376c1a69f650822bf19653232effc79e17d0d
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-15 00:37:02 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.23 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,21 +2,14 @@ import java.util.*;
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
+        String[] result = new String[n];
         for (int i = 0; i < n; i++) {
-            String a1 = String.format("%" + n + "s", Integer.toBinaryString(arr1[i])).replace(' ', '0');
-            String a2 = String.format("%" + n + "s", Integer.toBinaryString(arr2[i])).replace(' ', '0');
-            StringBuilder sb = new StringBuilder();
-            sb.append(a2);
-            
-            for (int j = 0; j < n; j++) {
-                if (a2.charAt(j) != '1' && a1.charAt(j) == '1'){
-                    sb.setCharAt(j, '1');
-                } 
-            }
-            
-            answer[i] = sb.toString().replace("1", "#").replace("0", " ");
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            result[i] = String.format("%" + n + "s", result[i]);
+            result[i] = result[i].replace("0", " ");
+            result[i] = result[i].replace("1", "#");
         }
-        return answer;
+    
+        return result;
     }
 }
