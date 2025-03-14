aa3c01d628354a289a79431b4c4856f5c56fb069
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-14 22:17:06 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] arr) {
+        int lcm = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            lcm = getLcm(lcm, arr[i]);
+        }
+        return lcm;
+    }
+    
+    private int getGcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return getGcd(b, a % b);
+    }
+    
+    private int getLcm(int a, int b) {
+        return a * b / getGcd(a, b);
+    }
+}
