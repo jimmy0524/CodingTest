5c769b3bea85986ec610c0f01986e4e368cd7a3e
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-14 23:37:25 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.15 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        ArrayList<String> arr = new ArrayList<>();
-        if (words[0].length() < 1) {
-            return new int[]{1,1};
-        }
+        HashSet<String> arr = new HashSet<>();
         
         arr.add(words[0]);
     
         for (int i = 1; i < words.length; i++) {
-            if (words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1) || arr.contains(words[i]) || words[i].length() < 1) {
-                return new int[]{i % n + 1, (int) Math.ceil((i + 1) / (double) n)};
+            if (words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1) || !arr.add(words[i])) {
+                return new int[]{i % n + 1,(i / n) + 1};
             }
             arr.add(words[i]);
         }
