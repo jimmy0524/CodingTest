3c9564b73bdad282670de0f55587ec54ba39a840
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-17 23:54:07 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 172.37 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] elements) {
+        HashSet<Integer>[] set = new Hashset[elements.length];
+    
+        for (int i = 0; i < elements.length; i++) {
+            set[i] = new HashSet<>();
+            for (int j = 0; j < elements.length; j++) {
+                set[i].add
+            }
+        }
+        int answer = 0;
+        return answer;
+    }
+}
