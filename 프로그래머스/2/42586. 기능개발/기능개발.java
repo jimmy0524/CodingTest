13e003f85656de3c490428f1889a93d39331e0ac
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-24 17:56:47 +0900

    [level 2] Title: 기능개발, Time: 3.22 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,32 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> daysQueue = new LinkedList<>();
-        ArrayList<Integer> result = new ArrayList<>();
-
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0; i < progresses.length; i++) {
-            int days = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            daysQueue.add(days);
+            queue.add((int) Math.ceil((float)(100 - progresses[i]) / speeds[i]));
         }
-
-        while (!daysQueue.isEmpty()) {
-            int currentDay = daysQueue.poll(); 
-            int count = 1; 
-
-            while (!daysQueue.isEmpty() && daysQueue.peek() <= currentDay) {
+        while (!queue.isEmpty()) {
+            int count = 1;
+            int currentDay = queue.poll();
+            while (!queue.isEmpty() && queue.peek() <= currentDay) {
+                queue.poll();
                 count++;
-                daysQueue.poll();
             }
-
-            result.add(count);
+            arr.add(count);
         }
         
-        int[] answer = new int[result.size()];
-        
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
-        
-        return answer;
+        return arr.stream().mapToInt(i -> i).toArray();
     }
 }
