f3f185df02c64098da076d94f6ea3e7379e03ecf
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-28 06:20:35 +0900

    [level 2] Title: 타겟 넘버, Time: 5.64 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,11 +1,15 @@
 class Solution {
     static int answer = 0;
+    static int[] numbers;
+    static int target;
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
+        this.numbers = numbers;
+        this.target = target;
+        dfs(0, 0);
         return answer;
     }
     
-    public void dfs(int[] numbers, int target, int count, int sum) {
+    public void dfs(int count, int sum) {
         if (count == numbers.length) {
             if (sum == target) {
                 answer++;
@@ -13,7 +17,7 @@ class Solution {
             return;
         }
         
-        dfs(numbers, target, count + 1, sum + numbers[count]);
-        dfs(numbers, target, count + 1, sum - numbers[count]);
+        dfs(count + 1, sum + numbers[count]);
+        dfs(count + 1, sum - numbers[count]);
     }
 }
