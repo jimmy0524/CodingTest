0e11ceabcd4f5893825eece1e68fffeae3a2f7dc
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-03-31 15:36:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 503.45 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] answer = {0,0};
+        PriorityQueue<Integer> asc = new PriorityQueue<>();
+        PriorityQueue<Integer> desc = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < operations.length; i++) {
+            String[] arr = new String[2];
+            arr = operations[i].split(" ");
+            if (arr[0].startsWith("I")) { //큐에 숫자 삽입
+                asc.add(Integer.parseInt(arr[1]));
+                desc.add(Integer.parseInt(arr[1]));
+            } else { //D로 시작하는 것
+                if (!asc.isEmpty()) {
+                    if (arr[1].equals("-1")) { //최솟값 삭제
+                        asc.poll();
+                        desc.clear();
+                        desc.addAll(asc);
+                    } else { //1 최댓값 삭제
+                        desc.poll();
+                        asc.clear();
+                        asc.addAll(desc);
+                    }
+                } 
+             }
+          }
+        
+        if (!asc.isEmpty()) {
+            answer[1] = asc.peek();
+            answer[0] = desc.peek();
+        }
+        
+            
+        return answer;
+    }
+}
