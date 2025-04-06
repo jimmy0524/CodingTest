b15ce84b6d100c84f54c33a5885c89929cd475f4
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-04-07 05:49:37 +0900

    [level 1] Title: 체육복, Time: 2.77 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        List<Integer> lostList = Arrays.stream(lost).boxed().collect(Collectors.toList());
+        List<Integer> reserveList = Arrays.stream(reserve).boxed().collect(Collectors.toList());
+        
+        for (int r : reserve) {
+            if (lostList.contains(r)) {
+                lostList.remove(Integer.valueOf(r));
+                reserveList.remove(Integer.valueOf(r));
+            }
+        }
+        for (Integer r : reserveList) {
+            if (lostList.contains(r - 1)) {
+                lostList.remove(Integer.valueOf(r - 1));
+                continue;
+            }
+            
+            if (lostList.contains(r + 1)) {
+                lostList.remove(Integer.valueOf(r + 1));
+                continue;
+            }
+        }
+        
+        return answer - lostList.size();
+    }
+}
