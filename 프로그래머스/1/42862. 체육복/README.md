5a19d8dfc8daaf5f86374ec01ef6239fe8439a69
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-04-07 05:50:54 +0900

    [level 1] Title: 체육복, Time: 3.75 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 86.2 MB, 시간: 2.77 ms
+메모리: 78.1 MB, 시간: 3.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 05:49:35
+2025년 04월 07일 05:50:52
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -4,7 +4,6 @@ import java.util.stream.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n;
-        Arrays.sort(lost);
         Arrays.sort(reserve);
         List<Integer> lostList = Arrays.stream(lost).boxed().collect(Collectors.toList());
         List<Integer> reserveList = Arrays.stream(reserve).boxed().collect(Collectors.toList());
