c5b644c14bb488b5dc0aed76e8ae2538e2a92005
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-04-11 22:00:45 +0900

    [level 2] Title: 큰 수 만들기, Time: 68.86 ms, Memory: 99 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < number.length(); i++) {
+            char now = number.charAt(i);
+            while (!stack.isEmpty() && k > 0 && stack.peek() < now) {
+                stack.pop();
+                k--;
+            }
+            stack.push(now);
+        }
+        
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
