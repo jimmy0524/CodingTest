e552007163dbd7f4208772fa8afcf9f4f5cfe504
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-04-16 19:19:51 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.67 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,78 +1,53 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
+        int[][] keypad = {
+            {1,3}, //0
+            {0,0}, //1
+            {1,0}, //2
+            {2,0}, //3
+            {0,1}, //4
+            {1,1}, //5
+            {2,1}, //6
+            {0,2}, //7
+            {1,2}, //8
+            {2,2}  //9
+    };
         int[] left = {0,3};
         int[] right = {2,3};
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numbers.length; i++) {
-            int[] temp = findKey(numbers[i]);
             if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
                 sb.append("L");
-                left = temp.clone();
+                left = keypad[numbers[i]].clone();
                 continue;
             }
             if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) {
                 sb.append("R");
-                right = temp.clone();
+                right = keypad[numbers[i]].clone();
                 continue;
             }
             
-            int leftDistance = Math.abs(left[0] - temp[0]) + Math.abs(left[1] - temp[1]);
-            int rightDistance = Math.abs(right[0] - temp[0]) + Math.abs(right[1] - temp[1]);
+            int leftDistance = Math.abs(left[0] - keypad[numbers[i]][0]) + Math.abs(left[1] - keypad[numbers[i]][1]);
+            int rightDistance = Math.abs(right[0] - keypad[numbers[i]][0]) + Math.abs(right[1] - keypad[numbers[i]][1]);
             
             if (rightDistance == leftDistance) {
                 if (hand.equals("right")) {
                     sb.append("R");
-                    right = temp.clone();
+                    right = keypad[numbers[i]].clone();
                     continue;
                 }
                 sb.append("L");
-                left = temp.clone();
+                left = keypad[numbers[i]].clone();
                 continue;
             } else if (rightDistance < leftDistance) {
                 sb.append("R");
-                right = temp.clone();
+                right = keypad[numbers[i]].clone();
                 continue;
             }
             sb.append("L");
-            left = temp.clone();
+            left = keypad[numbers[i]].clone();
             continue;
         }
         return sb.toString();
-    }
-    public int[] findKey(int n) {
-        int[] temp = new int[2];
-        switch (n) {
-            case 1:
-                temp = new int[]{0, 0};
-                break;
-            case 2:
-                temp = new int[]{1, 0};
-                break;
-            case 3:
-                temp = new int[]{2, 0};
-                break;
-            case 4:
-                temp = new int[]{0, 1};
-                break;
-            case 5:
-                temp = new int[]{1, 1};
-                break;
-            case 6:
-                temp = new int[]{2, 1};
-                break;    
-            case 7:
-                temp = new int[]{0, 2};
-                break;    
-            case 8:
-                temp = new int[]{1, 2};
-                break;
-            case 9:
-                temp = new int[]{2, 2};
-                break;
-            case 0:
-                temp = new int[]{1, 3};
-                break;
-        }
-        return temp;
     }
 }
