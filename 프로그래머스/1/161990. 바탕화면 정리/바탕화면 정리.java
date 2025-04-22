46ce2d36214a169758e5f23088bda234ca254209
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-04-22 19:33:18 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.29 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,50 +1,21 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
-        int as = 0;
-        int ae = 0;
-        int bs = 0;
-        int be = 0;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
         here:
         for (int i = 0; i < wallpaper.length; i++) {
             for (int j = 0; j < wallpaper[i].length(); j++) {
                 if(wallpaper[i].charAt(j) == '#') {
-                    as = i;
-                    break here;
+                    minX = Math.min(minX, i);
+                    minY = Math.min(minY, j);
+                    maxX = Math.max(maxX, i);
+                    maxY = Math.max(maxY, j);
                 }
             }
         }
         
-        here:
-        for (int j = 0; j < wallpaper[0].length(); j++) {
-          for (int i = 0; i < wallpaper.length; i++) {
-                if(wallpaper[i].charAt(j) == '#') {
-                    ae = j;
-                    break here;
-                }
-            }
-        }
-        
-        here:
-        for (int i = wallpaper.length - 1; i >= 0; i--) {
-            for (int j = wallpaper[i].length() - 1; j >= 0; j--) {
-                if(wallpaper[i].charAt(j) == '#') {
-                    bs = i + 1;
-                    break here;
-                }
-            }
-        }
-        
-        here:
-        for (int j = wallpaper[0].length() - 1; j >= 0; j--) {
-            for (int i = wallpaper.length - 1; i >= 0; i--) {
-                if(wallpaper[i].charAt(j) == '#') {
-                    be = j + 1;
-                    break here;
-                }
-            }
-        }
-        
-        return new int[]{as, ae, bs, be};
+        return new int[]{minX, minY, maxX + 1, maxY + 1};
     }
 }
