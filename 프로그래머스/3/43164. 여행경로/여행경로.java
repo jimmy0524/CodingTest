69ca1c801894e6834019de7b0c396dae017bebda
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-04-26 01:16:22 +0900

    [level 3] Title: 여행경로, Time: 57.93 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    boolean[] visited;
+    List<String> answer = new ArrayList<>();
+    String[][] tickets;
+    public String[] solution(String[][] tickets) {
+        this.tickets = tickets;
+        visited = new boolean[tickets.length];
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        
+        dfs("ICN", 0, new StringBuilder());
+        
+        return answer.get(0).trim().split(" ");
+    }
+    public void dfs(String s, int depth, StringBuilder sb) {
+        if (sb.length() < 1) {
+            sb.append(s + " ");
+        }
+        if (depth == tickets.length) {
+            if (answer.isEmpty()) {
+                answer.add(sb.toString());
+            }
+            return;
+        }
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && tickets[i][0].equals(s)) {
+                visited[i] = true;
+                int tempLength = sb.length();
+                sb.append(tickets[i][1] + " ");
+                dfs(tickets[i][1], depth + 1, sb);
+                sb.setLength(tempLength);   
+                visited[i] = false;
+            }
+        }
+    }
+}
