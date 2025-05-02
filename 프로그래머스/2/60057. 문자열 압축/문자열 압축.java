35f28b4f5019b2855b2f6ba3a40b800cc409c050
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-05-02 21:26:49 +0900

    [level 2] Title: 문자열 압축, Time: 7.60 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        if (s.length() == 1) {
+            return 1;
+        } 
+        int answer = Integer.MAX_VALUE;
+        int length = s.length() / 2;
+        for (int i = 1; i <= length; i++) {
+            StringBuilder sb = new StringBuilder();
+            Queue<String> q = new LinkedList<>();
+            for (int j = 0; j <= s.length() - i; j += i) {
+                q.add(s.substring(j, j + i));
+            }
+            int remainNum = s.length() - (q.size() * i);
+            String remain = s.substring(s.length() - remainNum, s.length());
+            while(!q.isEmpty()) {
+                int count = 1;
+                String temp = q.poll();
+                while (!q.isEmpty() && temp.equals(q.peek())) {
+                    q.poll();
+                    count++;
+                }
+                if (count == 1) {
+                    sb.append(temp);
+                } else {
+                    sb.append(String.valueOf(count));
+                    sb.append(temp);
+                }
+                count = 1;
+            }
+            sb.append(remain);
+            answer = Math.min(sb.length(), answer);
+        }
+        return answer;
+    }
+}
