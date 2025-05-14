26920e6b0a0bee3a74a45c2caebbf59fd124cc17
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-05-14 20:12:34 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,28 +1,26 @@
-import java.io.*;
 import java.util.*;
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        int[] dp = new int[k + 1];
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+import java.io.*;
 
-        dp[0] = 1;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 1; j <= k; j++) {
-                if (j >= arr[i]) {
-                    dp[j] = dp[j] + dp[j - arr[i]];
-                }
-            }
-        }
-
-
-        System.out.println(dp[k]);
-    }
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		int[] dp = new int[k + 1];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		dp[0] = 1;
+		for (int j = 0; j < n; j++) {
+			for (int i = 1; i <= k; i++) {
+				if (i - arr[j] < 0) {
+					continue;
+				}
+				dp[i] += dp[i - arr[j]];
+			}
+		}
+		System.out.println(dp[k]);
+	}
 }
