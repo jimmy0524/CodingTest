8234dbe3cbf8d17021f8540da170eb88db1be134
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-05-16 02:29:28 +0900

    [Silver I] Title: 1로 만들기 2, Time: 140 ms, Memory: 23328 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -6,49 +6,31 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] dp = new int[n + 1];
-		ArrayList<Integer>[] arr = new ArrayList[n + 1];
-		for (int i = 1; i <= n; i++) {
-			arr[i] = new ArrayList<>();
-		}
-		arr[1].add(1);
+		int[] before = new int[n + 1];
+
 		for (int i = 2; i <= n; i++) {
 			dp[i] = dp[i - 1] + 1;
+			before[i] = i - 1;
 			int what = 1;  
 			if (i % 2 == 0) {
 				int two = dp[i / 2] + 1;
 				if (dp[i] > two) {
-					what = 2;
+					before[i] = i / 2;
 					dp[i] = two;
 				}
 			}
 			if (i % 3 == 0) {
 				int three = dp[i / 3] + 1;
 				if (dp[i] > three) {
-					what = 3;
+					before[i] = i / 3;
 					dp[i] = three;
 				}
 			}
-			switch (what) {
-			case 1 :
-				for (int j : arr[i - 1]) {
-					arr[i].add(j);
-				}
-				break;
-			case 2 : 
-				for (int j : arr[i / 2]) {
-					arr[i].add(j);
-				}
-				break;
-			case 3 : 
-				for (int j : arr[i / 3]) {
-					arr[i].add(j);
-				}
-			}
-			arr[i].add(i);
 		}
 		System.out.println(dp[n]);
-		for (int i = arr[n].size() - 1; i >= 0; i--) {
-			System.out.print(arr[n].get(i) + " ");
+		while (n > 0) {
+			System.out.print(n + " ");
+			n = before[n];
 		}
 	}
 }
