ed5e05a3f7ce4774071b8aa40a35fbe1a19b1ee4
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-05-16 21:00:35 +0900

    [Silver IV] Title: 로프, Time: 292 ms, Memory: 26328 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,20 +1,22 @@
-import java.io.*;
 import java.util.*;
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Integer[] arr = new Integer[N];
-        int result = 0;
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr, Collections.reverseOrder());
+import java.io.*;
 
-        for (int i = 0; i < N; i++) {
-            result = Math.max(arr[i] * (i + 1), result);
-        }
-
-        System.out.println(result);
-    }
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		int answer = Integer.MIN_VALUE;
+		for (int i = 0; i < N; i++) {
+			int now = arr[i] * (N - i);
+			if (answer < now) {
+				answer = now;
+			} 
+		}
+		System.out.println(answer);
+	}
 }
