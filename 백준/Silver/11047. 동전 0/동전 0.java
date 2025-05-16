3ba2d2376a2a06adb8d1c821d260a2ff056c38d1
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-05-16 19:42:17 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -12,13 +12,10 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-		while(K > 0) {
-			for (int i = N - 1; i >= 0; i--) {
-				if (arr[i] <= K) {
-					answer += K / arr[i];
-					K %= arr[i];
-					break;
-				}
+		for (int i = N - 1; i >= 0; i--) {
+			if (arr[i] <= K) {
+				answer += K / arr[i];
+				K %= arr[i];
 			}
 		}
 		System.out.println(answer);
