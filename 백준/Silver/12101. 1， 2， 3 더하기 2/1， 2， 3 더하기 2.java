783d95b56b882bb1d7e40daf342cd73e9a7ffa8b
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-06-01 20:41:28 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -2,36 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int n;
+	static int k;
+	static int count;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		ArrayList<String>[] dp = new ArrayList[n + 3];
-		for (int i = 0; i <= n + 2; i++) {
-			dp[i] = new ArrayList<>();
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		count = k;
+		back(0, new ArrayList<>());
+		if (count != 0) {
+			System.out.println(-1);
 		}
-		dp[1].add("1");
-		dp[2].add("1+1");
-		dp[2].add("2");
-		dp[3].add("1+2");
-		dp[3].add("2+1");
-		dp[3].add("1+1+1");
-		dp[3].add("3");
+	}
+	
+	public static void back(int sum, ArrayList<Integer> arr) {
+		if (count == 0) {
+			return;
+		}
 		
-		for (int i = 4; i <= n; i++) {
-			for (int j = 1; j <= 3; j++) {
-				for (String s : dp[i - j]) {
-					dp[i].add(s + "+" + j);
+		if (sum > n) {
+			return;
+		}
+		
+		if (sum == n) {
+			count--;
+			if (count == 0) {
+				StringBuilder sb = new StringBuilder();
+				for (int a : arr) {
+					sb.append(a);
+					sb.append("+");
 				}
+				sb.deleteCharAt(sb.length() - 1);
+				System.out.println(sb.toString());
+				return;
+			} else {
+				return;
 			}
 		}
 		
-		if (dp[n].size() < k) {
-			System.out.println(-1);
-		} else {
-			Collections.sort(dp[n]);
-			System.out.println(dp[n].get(k - 1));
+		for (int i = 1; i <= 3; i++) {
+			arr.add(i);
+			back(sum + i, arr);
+			arr.remove(arr.size() - 1);
 		}
 	}
 }
