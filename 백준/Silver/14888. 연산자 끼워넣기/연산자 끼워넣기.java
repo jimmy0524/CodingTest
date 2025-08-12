f2e5cf608a9ce8b4edcca2fd76d9884e8f5ef3a4
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-08-12 14:42:47 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,71 +1,59 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int N;
 	static int[] num;
-	static int[] math;
-	static int min = Integer.MAX_VALUE;
+	static int[] calNum;
 	static int max = Integer.MIN_VALUE;
-	public static void main(String args[]) throws Exception {
+	static int min = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine()); //수의 개수
+		N = Integer.parseInt(br.readLine());
 		num = new int[N];
+		calNum = new int[4];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
 		st = new StringTokenizer(br.readLine());
-		math = new int[4];
 		for (int i = 0; i < 4; i++) {
-			math[i] = Integer.parseInt(st.nextToken());
+			calNum[i] = Integer.parseInt(st.nextToken());
 		}
+		dfs(0, num[0]);
 		
-		dfs(new ArrayList<>());
 		System.out.println(max);
 		System.out.println(min);
 	}
 	
-	private static void dfs(ArrayList<Integer> q) {
-		if (q.size() == N - 1) {
-			int result = num[0]; 
-			for (int i = 1; i < N; i++) {
-				int b = num[i]; 
-				int m = q.get(i - 1);
-				switch (m) {
-				case 0:
-					result += b;
-					break;
-				case 1:
-					result -= b;
-					break;
-				case 2:
-					result *= b;
-					break;
-				case 3:
-					result /= b;
-				}
-			}
-			max = Math.max(result, max);
-			min = Math.min(result, min);
-			
+	public static void dfs(int index, int sum) {
+		if (index >= N - 1) {
+			max = Math.max(max, sum);
+			min = Math.min(min, sum);
 			return;
 		}
-		
 		for (int i = 0; i < 4; i++) {
-			if (math[i] > 0) {
-				math[i] -= 1;
-				q.add(i);
-				dfs(q);
-				q.remove(q.size() - 1);
-				math[i] += 1;
+			if (calNum[i] > 0) {
+				calNum[i]--;
+				int tempSum = calculate(i, sum, index);
+				dfs(index + 1, tempSum);
+				calNum[i]++;
 			}
 		}
+	}
+	
+	public static int calculate(int cal, int sum, int index) {
+		switch(cal) {
+		case 0 : 
+			return sum + num[index + 1];
+		case 1 : 
+			return sum - num[index + 1];
+		case 2 : 
+			return sum * num[index + 1];
+		case 3 : 
+			return sum / num[index + 1];
+		}
+		return 0;
 	}
 }
