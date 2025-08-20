2da9ebb09403d1615ad29bfb9155a42e0e3ef1cc
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-08-20 14:08:15 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,65 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static boolean[][] visited;
-	static int[][] arr;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
-	static int N;
-	static int M;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); //정점 개수
-		M = Integer.parseInt(st.nextToken()); //간선 개수
-		arr = new int[N][M];
-		visited = new boolean[N][M];
-		for (int i = 0; i < N; i++) {
-			String s = br.readLine();
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = s.charAt(j) - '0';
-			}
-		}
-		
-		System.out.println(bfs());
-	}
-	
-	private static int bfs() {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0, 0));
-		visited[0][0] = true;
-		int answer = 0;
-		
-		while(!q.isEmpty()) {
-			int size = q.size();
-			answer++;
-			for (int j = 0; j < size; j++) {
-				Node now = q.poll();
-				if (now.x == N - 1 && now.y == M - 1) {
-					return answer;
-				}
-				for (int i = 0; i < 4; i++) {
-					int nx = now.x + dx[i];
-					int ny = now.y + dy[i];
-					if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1) {
-						if (arr[nx][ny] == 1 && !visited[nx][ny]) {
-							q.add(new Node(nx, ny));
-							visited[nx][ny] = true;
-						}
-					}
-				}
-			}
-		}
-		
-		return answer;
-	}
-	
-	private static class Node {
-		int x, y;
-		
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+    static int N, M;
+    static int[][] arr;
+    static final int[] dx = { 1, -1, 0, 0 };
+    static final int[] dy = { 0, 0, -1, 1 };
+    static int answer;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        bfs();
+
+        System.out.println(answer);
+    }
+
+    public static void bfs() {
+        Queue<Node> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][M];
+        q.add(new Node(0, 0));
+        visited[0][0] = true;
+        int depth = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                Node now = q.poll();
+                if (now.x == N - 1 && now.y == M - 1) {
+                    answer = depth;
+                    return;
+                }
+                for (int k = 0; k < 4; k++) {
+                    int nx = now.x + dx[k];
+                    int ny = now.y + dy[k];
+                    if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && arr[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        q.add(new Node(nx, ny));
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Node {
+        int x, y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
