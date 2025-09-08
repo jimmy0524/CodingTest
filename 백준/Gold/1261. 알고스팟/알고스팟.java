4a567419441651597e8cc76a46ff83b85b4cae6f
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2025-09-08 11:03:30 +0900

    [Gold IV] Title: 알고스팟, Time: 116 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -4,11 +4,9 @@ import java.io.*;
 public class Main {
     static int[][] arr;
     static int[][] value;
-    static boolean[][] visited;
     static int[] dx = { 1, -1, 0, 0 };
     static int[] dy = { 0, 0, 1, -1 };
     static int N, M;
-    static PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.value - b.value);
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,7 +16,6 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         arr = new int[M][N];
         value = new int[M][N];
-        visited = new boolean[M][N];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -34,37 +31,30 @@ public class Main {
     }
 
     static void disk() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[] { 0, 0 });
         value[0][0] = 0;
-        pq.add(new Node(0, 0, 0));
 
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
-            if (!visited[now.toX][now.toY]) {
-                visited[now.toX][now.toY] = true;
+        while (!dq.isEmpty()) {
+            int[] now = dq.poll();
+            int x = now[0];
+            int y = now[1];
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if (nx < 0 || ny < 0 || nx >= M || ny >= N)
+                    continue;
 
-                for (int k = 0; k < 4; k++) {
-                    int nx = now.toX + dx[k];
-                    int ny = now.toY + dy[k];
-                    if (nx < 0 || ny < 0 || nx >= M || ny >= N)
-                        continue;
-
-                    int cost = value[now.toX][now.toY] + arr[nx][ny];
-                    if (value[nx][ny] > cost) {
-                        value[nx][ny] = cost;
-                        pq.add(new Node(nx, ny, cost));
+                int cost = value[x][y] + arr[nx][ny];
+                if (value[nx][ny] > cost) {
+                    value[nx][ny] = cost;
+                    if (arr[nx][ny] == 0) {
+                        dq.addFirst(new int[] { nx, ny });
+                    } else {
+                        dq.addLast(new int[] { nx, ny });
                     }
                 }
             }
-        }
-    }
-
-    static class Node {
-        int toX, toY, value;
-
-        Node(int toX, int toY, int value) {
-            this.toX = toX;
-            this.toY = toY;
-            this.value = value;
         }
     }
 }
