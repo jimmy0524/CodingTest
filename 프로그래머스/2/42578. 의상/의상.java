cf3139e4bad1589ae3cbe9684667d3cde5a387f8
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2026-03-07 17:34:37 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
-        int answer = 1;
         for (int i = 0; i < clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);  
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
+        int answer = 1;
         for (int count : map.values()) {
-            answer *= (count + 1);
+            answer *= count + 1;
         }
         return answer - 1;
     }
