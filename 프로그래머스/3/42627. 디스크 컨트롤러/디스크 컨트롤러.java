54fabc3c4fa8535e45d057319018273cb2f24d08
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2026-03-08 21:42:43 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.38 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,26 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        int count = 0;
-        int time = 0;
-        int returnTime = 0;
-        int index = 0;
         
-        while(count < jobs.length) {
-            while (index < jobs.length && jobs[index][0] <= time) {
-                pq.add(jobs[index++]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[1];
+            } else {
+                return a[1] - b[1];
+            }
+        });
+        
+        int sum = 0;
+        int time = 0;
+        int idx = 0; //다음에 들어올 작업 인덱스
+        int count = 0; //처리한 작업 수
+        while (count < jobs.length) {
+            while (idx < jobs.length && jobs[idx][0] <= time) {
+                pq.add(jobs[idx]);
+                idx++;
             }
             
             if (!pq.isEmpty()) {
-                int[] job = pq.poll();
-                time += job[1];
-                returnTime += (time - job[0]);
+                int[] now = pq.poll();
+                time += now[1];
+                sum += (time - now[0]); // 종료시간 - 요청시간
                 count++;
             } else {
-                time = jobs[index][0];
+                // 현재 도착한 작업이 없으면 시간을 다음 작업 요청 시각으로 이동
+                time = jobs[idx][0];
             }
         }
-        return returnTime / jobs.length;
+        return sum / jobs.length;
     }
 }
