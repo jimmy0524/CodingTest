1c96b220f0384d6117f9c410a20ae9064df4579d
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2026-03-09 17:34:43 +0900

    [Gold IV] Title: 행성 연결, Time: 1072 ms, Memory: 121608 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.java
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -1,62 +1,69 @@
-import java.util.*;
-import java.io.*;
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[] parent;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		long answer = 0;
-		ArrayList<Node> arr = new ArrayList<>();
-		parent = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				int now = Integer.parseInt(st.nextToken());
-				arr.add(new Node(i, j, now));
-			}
+    static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Node> arr = new ArrayList<>();
+        parent = new int[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             parent[i] = i;
-		}
-		arr.sort((a, b) -> a.value - b.value);
-		int count = 0;
-		for (int i = 0; i < arr.size(); i++) {
-			if (find(arr.get(i).to) != find(arr.get(i).from)) {
-				count++;
-				answer += arr.get(i).value;
-				union(arr.get(i).to, arr.get(i).from);
-				if (count == N - 1) {
-					break;
-				}
-			}
-		}
-		System.out.println(answer);
-	}
-	
-	public static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if (a != b) {
-			if (a > b) {
-				parent[a] = b;
-			} else {
-				parent[b] = a;
-			}
-		}
-	}
-	
-	public static int find(int a) {
-		if (parent[a] == a) {
-			return a;
-		}
-		return parent[a] = find(parent[a]);
-	}
-	
-	public static class Node {
-		int from, to, value;
-		public Node(int from, int to, int value) {
-			this.from = from;
-			this.to = to;
-			this.value = value;
-		}
-	}
+            for (int j = 0; j < N; j++) {
+                int value = Integer.parseInt(st.nextToken());
+                if (i > j) {
+                    arr.add(new Node(i, j, value));
+                }
+            }
+        }
+
+        arr.sort((a,b) -> a.value - b.value);
+
+        long answer = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            Node now = arr.get(i);
+            if (find(now.start) != find(now.end)) {
+                union(now.start, now.end);
+                answer += now.value;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            if (a < b) {
+                parent[b] = a;
+            } else {
+                parent[a] = b;
+            }
+        }
+    }
+
+    public static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    public static class Node {
+        int start, end, value;
+        public Node (int start, int end, int value) {
+            this.start = start;
+            this.end = end;
+            this.value = value;
+        }
+    }
 }
