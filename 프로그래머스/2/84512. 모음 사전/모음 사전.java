56bd6219403ad49198b6472faa1a521192e2d98c
Jimin Lim <98664227+jimmy0524@users.noreply.github.com>
2026-03-28 02:24:35 +0900

    [level 2] Title: 모음 사전, Time: 16.49 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,37 +1,24 @@
-import java.util.*;
-
 class Solution {
+    int answer;
+    String word;
     char[] alpha = {'A', 'E', 'I', 'O', 'U'};
-    ArrayList<String> arr = new ArrayList<>();
-    
     public int solution(String word) {
-        int answer = 0;
-        dfs("", 0);
-        
-        if (arr.get(0).equals("")) {
-            arr.remove(0);
-        }
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i).equals(word)) {
-                answer = i + 1;
-            }
-        }
-
+        answer = 0;
+        this.word = word;
+        dfs("");
         return answer;
     }
     
-    public void dfs(String temp, int index) {
-        if (index == 5) {
-            arr.add(temp);
-            return;
-        }
-        
-        if (temp.length() >= 1) {
-            arr.add(temp);
-        }
+    public boolean dfs(String s) {
+        if (s.equals(word)) return true;
+        if (s.length() == 5) return false;
         
         for (int i = 0; i < 5; i++) {
-            dfs(temp + alpha[i], index + 1);
+            answer++;
+            if (dfs(s + alpha[i])) {
+                return true;
+            }
         }
+        return false;
     }
 }
